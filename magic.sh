57dd4e7edee7ab3d677cef3c267572d607901983
Ryan Sears <rdsears@Ryan-Searss-MacBook-Pro-17.local>
2012-07-17 14:42:47 -0400

    Add some comments to magic.sh

--- magic.sh
+++ magic.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+#
+# Runs test.py through the patched pyinstaller, bundling everything into a single executable, then running it making sure it works
+# As long as you don't see a traceback everything is groovy
+#
 
 cd pyinstaller
 echo "Running  patched pyinstaller"
