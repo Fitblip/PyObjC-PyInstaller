0a167b1261a41df1f74657b2942773f8dea0f720
Ryan Sears <rdsears@Ryan-Searss-MacBook-Pro-17.local>
2012-07-19 14:55:58 -0400

    upload an actual binary to test

--- magic.sh
+++ magic.sh
@@ -9,4 +9,4 @@ echo "Running  patched pyinstaller"
 /usr/bin/python pyinstaller.py --PyObjC --onefile ../test.py
 echo "Examining clipboard types with PyObjC bindings"
 test/dist/test
-rm -rf test/
+#rm -rf test/
